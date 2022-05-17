eb1fbf1665b8c01d38c04879da830f12d6d97bdf
mkay47 <201402344@student.uj.ac.za>
2022-05-17 15:03:53 +0200

    init commit

---
+++ discovery-service/src/main/java/com/example/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(DiscoveryServiceApplication.class, args);
+		new SpringApplicationBuilder(DiscoveryServiceApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+
+}
